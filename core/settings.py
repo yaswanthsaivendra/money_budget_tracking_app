ed135b1c674177e0c98a21b01d30fa8385ce78b7
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2023-11-02 14:39:35 +0530

    added staticfiles conf

--- core/settings.py
+++ core/settings.py
@@ -128,6 +128,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- core/urls.py
+++ core/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('accounts.urls')),
     path('splitter/', include('splitter.urls'))
 ]
+
+urlpatterns += staticfiles_urlpatterns() # new
